2551a26ca2f312371cb21e6a36092d6e94941b56
yejini.lee <yejini.lee@lge.com>
2015-09-17 08:59:18 +0900

    input: misc: fpc: add irq sysfs node
    replace the input device interface with a new sysfs node "irq"
    that will return the current status of the interrupt io.
    For this patch, TTW latency can be reeduced by 100msec
    
    Bug: 23725572
    
    Change-Id: I5ef3b82872eb8c7ad4bf200612e5d86e39a06e88
    Signed-off-by: yejini.lee <yejini.lee@lge.com>

--- drivers/input/fingerprint/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc1020_tee.c
@@ -34,7 +34,6 @@
 #include <linux/clk.h>
 #include <linux/delay.h>
 #include <linux/gpio.h>
-#include <linux/input.h>
 #include <linux/interrupt.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -67,12 +66,7 @@ struct fpc1020_data {
 	struct clk *core_clk;
 	int irq_gpio;
 	int rst_gpio;
-	struct input_dev *idev;
-	int irq_num;
 	int qup_id;
-	char idev_name[32];
-	int event_type;
-	int event_code;
 	struct mutex lock;
 	bool prepared;
 	int  wakeup_enabled;
@@ -320,6 +314,27 @@ exit:
 	return rc;
 }
 
+/* sysfs node to check the interrupt status of the sensor */
+static ssize_t irq_get(struct device *device,
+			     struct device_attribute *attribute,
+			     char *buffer)
+{
+	struct fpc1020_data *fpc1020 = dev_get_drvdata(device);
+	int irq = gpio_get_value(fpc1020->irq_gpio);
+	return scnprintf(buffer, PAGE_SIZE, "%i\n", irq);
+}
+
+/* write to the irq node */
+static ssize_t irq_ack(struct device *device,
+			     struct device_attribute *attribute,
+			     const char *buffer, size_t count)
+{
+	struct fpc1020_data *fpc1020 = dev_get_drvdata(device);
+	dev_dbg(fpc1020->dev, "%s\n", __func__);
+	return count;
+}
+static DEVICE_ATTR(irq, S_IRUSR | S_IWUSR, irq_get, irq_ack);
+
 static ssize_t hw_reset_set(struct device *dev,
 	struct device_attribute *attr, const char *buf, size_t count)
 {
@@ -414,6 +429,7 @@ static ssize_t spi_prepare_set(struct device *dev,
 static DEVICE_ATTR(spi_prepare, S_IWUSR, NULL, spi_prepare_set);
 
 static struct attribute *attributes[] = {
+    &dev_attr_irq.attr,
 	&dev_attr_pinctl_set.attr,
 	&dev_attr_clk_enable.attr,
 	&dev_attr_spi_owner.attr,
@@ -431,19 +447,18 @@ static const struct attribute_group attribute_group = {
 static irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 {
 	struct fpc1020_data *fpc1020 = handle;
+	dev_dbg(fpc1020->dev, "%s\n", __func__);
 
 	/* Make sure 'wakeup_enabled' is updated before using it
 	** since this is interrupt context (other thread...) */
 	smp_rmb();
 
-	if (fpc1020->wakeup_enabled) {
+	if (fpc1020->wakeup_enabled ) {
 		wake_lock_timeout(&fpc1020->ttw_wl, msecs_to_jiffies(FPC_TTW_HOLD_TIME));
-		/*dev_info(fpc1020->dev, "%s - wake_lock_timeout\n", __func__);*/
 	}
 
-	input_event(fpc1020->idev, EV_MSC, MSC_SCAN, ++fpc1020->irq_num);
-	input_sync(fpc1020->idev);
-	dev_dbg(fpc1020->dev, "%s %d\n", __func__, fpc1020->irq_num);
+	sysfs_notify(&fpc1020->dev->kobj, NULL, dev_attr_irq.attr.name);
+
 	return IRQ_HANDLED;
 }
 
@@ -475,7 +490,6 @@ static int fpc1020_probe(struct spi_device *spi)
 	int irqf;
 	struct device_node *np = dev->of_node;
 	u32 val;
-	const char *idev_name;
 	struct fpc1020_data *fpc1020 = devm_kzalloc(dev, sizeof(*fpc1020),
 			GFP_KERNEL);
 	if (!fpc1020) {
@@ -565,34 +579,6 @@ static int fpc1020_probe(struct spi_device *spi)
 	rc = select_pin_ctl(fpc1020, "spi12_fpc_active");
 	if (rc)
 		goto exit;
-
-	rc = of_property_read_u32(np, "fpc,event-type", &val);
-	fpc1020->event_type = rc < 0 ? EV_MSC : val;
-
-	rc = of_property_read_u32(np, "fpc,event-code", &val);
-	fpc1020->event_code = rc < 0 ? MSC_SCAN : val;
-
-	fpc1020->idev = devm_input_allocate_device(dev);
-	if (!fpc1020->idev) {
-		dev_err(dev, "failed to allocate input device\n");
-		rc = -ENOMEM;
-		goto exit;
-	}
-	input_set_capability(fpc1020->idev, fpc1020->event_type,
-			fpc1020->event_code);
-
-	if (!of_property_read_string(np, "input-device-name", &idev_name)) {
-		fpc1020->idev->name = idev_name;
-	} else {
-		snprintf(fpc1020->idev_name, sizeof(fpc1020->idev_name),
-			"fpc1020@%s", dev_name(dev));
-		fpc1020->idev->name = fpc1020->idev_name;
-	}
-	rc = input_register_device(fpc1020->idev);
-	if (rc) {
-		dev_err(dev, "failed to register input device\n");
-		goto exit;
-	}
 
 	wake_lock_init(&fpc1020->ttw_wl, WAKE_LOCK_SUSPEND, "fpc_ttw_wl");
 
