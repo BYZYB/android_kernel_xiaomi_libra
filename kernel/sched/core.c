7a6288ead5e1f74798270319e6524bc7895866d2
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2016-02-14 11:46:09 +0100

    EAS: disable debug message
    excessive log trigger watchdog
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8973,8 +8973,8 @@ sched_set_cpu_cstate(int cpu, int cstate, struct cpuidle_state *cpuidle,
 	    (cstate == 3 && cpu >= 4 && wakeup_energy == 155775 && wakeup_latency == 475) ||
 	    (cstate == 4 && cpu < 4 && wakeup_energy == 46400 && wakeup_latency == 480) ||
 	    (cstate == 4 && cpu >= 4 && wakeup_energy == 192156 && wakeup_latency == 636)) {
-	} else {
-		pr_info("TJK: CPU%d ->cstate=%d  nrg=%d latency=%d\n", cpu, cstate, wakeup_energy, wakeup_latency);
+//	} else {
+//		pr_info("TJK: CPU%d ->cstate=%d  nrg=%d latency=%d\n", cpu, cstate, wakeup_energy, wakeup_latency);
 	}
 }
 
