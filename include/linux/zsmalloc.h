3ec602fe7489d69e0c3d93e12434f9fe2049230c
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2016-07-26 15:22:54 -0700

    zram: delete custom lzo/lz4
    Remove lzo/lz4 backends, we use crypto API now.
    
    [sergey.senozhatsky@gmail.com: zram-delete-custom-lzo-lz4-v3]
      Link: http://lkml.kernel.org/r/20160604024902.11778-6-sergey.senozhatsky@gmail.com
    Link: http://lkml.kernel.org/r/20160531122017.2878-7-sergey.senozhatsky@gmail.com
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Change-Id: I02882ad4bcf3058cca0135a0a99b24551ec9f592

--- include/linux/zsmalloc.h
+++ include/linux/zsmalloc.h
@@ -36,17 +36,15 @@ enum zs_mapmode {
 
 struct zs_pool;
 
-struct zs_pool *zs_create_pool(char *name, gfp_t flags);
+struct zs_pool *zs_create_pool(char *name);
 void zs_destroy_pool(struct zs_pool *pool);
 
-unsigned long zs_malloc(struct zs_pool *pool, size_t size);
+unsigned long zs_malloc(struct zs_pool *pool, size_t size, gfp_t flags);
 void zs_free(struct zs_pool *pool, unsigned long obj);
 
 void *zs_map_object(struct zs_pool *pool, unsigned long handle,
 			enum zs_mapmode mm);
 void zs_unmap_object(struct zs_pool *pool, unsigned long handle);
-int zs_get_page_usage(unsigned long *total_pool_pages,
-			unsigned long *total_ori_pages);
 
 unsigned long zs_get_total_pages(struct zs_pool *pool);
 unsigned long zs_compact(struct zs_pool *pool);
