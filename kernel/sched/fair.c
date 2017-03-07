2d8c716935355ccd6459f68f3b474061d81a8351
Greg Hackmann <ghackmann@google.com>
2017-03-07 10:37:56 -0800

    ANDROID: sched: fix duplicate sched_group_energy const specifiers
    EAS uses "const struct sched_group_energy * const" fairly consistently.
    But a couple of places swap the "*" and second "const", making the
    pointer mutable.
    
    In the case of struct sched_group, "* const" would have been an error,
    since init_sched_energy() writes to sd->groups->sge.
    
    Change-Id: Ic6a8fcf99e65c0f25d9cc55c32625ef3ca5c9aca
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4601,7 +4601,8 @@ long group_norm_util(struct energy_env *eenv, struct sched_group *sg)
 	return util_sum;
 }
 
-static int find_new_capacity(struct energy_env *eenv, const struct sched_group_energy const *sge)
+static int find_new_capacity(struct energy_env *eenv,
+	const struct sched_group_energy * const sge)
 {
 	int idx;
 	unsigned long util = group_max_util(eenv);
