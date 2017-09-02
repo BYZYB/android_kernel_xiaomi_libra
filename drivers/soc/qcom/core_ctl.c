cc1ea9caa0227a6b6dc2f877ca70b5324bea58a5
RJ Trujillo <CertifiedBlyndGuy@gmail.com>
2017-09-02 21:12:04 +0200

    core_ctl: Fix elapsed time calculation
    * ISO C90 forbids mixed declarations and code as it's easier to read the declaration
      and then the assigned code
    * If elapsed is called later and assigned elsewhere then it looks out of place
    
    Signed-off-by: Joe Maples <joe@frap129.org>
    Change-Id: I1ac164c54316b531b076f705cbf69fb2ae95e013

--- drivers/soc/qcom/core_ctl.c
+++ drivers/soc/qcom/core_ctl.c
@@ -650,7 +650,7 @@ static bool eval_need(struct cpu_data *f)
 	unsigned int need_cpus = 0, last_need, thres_idx;
 	int ret = 0;
 	bool need_flag = false;
-	s64 now;
+	s64 elapsed, now;
 
 	if (unlikely(!f->inited))
 		return 0;
@@ -676,7 +676,8 @@ static bool eval_need(struct cpu_data *f)
 		return 0;
 	}
 
-	s64 elapsed = now - f->need_ts;
+	elapsed = now - f->need_ts;
+
 	if (need_cpus > last_need) {
 		if (elapsed >= f->online_delay_ms) {
 			ret = 1;
