6b7e0c3abd05b90bad6bf589e2c6012198a21ff3
Peter Zijlstra <peterz@infradead.org>
2018-08-29 11:45:21 +0200

    BACKPORT: sched/debug: Explicitly cast sched_feat() to bool
    LLVM has a warning that tags expressions like:
    
    	if (foo && non-bool-const)
    
    This pattern triggers for CONFIG_SCHED_DEBUG=n where sched_feat() ends
    up being whatever bit we select. Avoid the warning with an explicit
    cast to bool.
    
    Reported-by: Philipp Klocke <philipp97kl@gmail.com>
    Tested-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://git.kernel.org/linus/7e6f4c5d600c1c8e2a1d900e65cab319d9b6782e
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Change-Id: I74938488b92b72dc52001773b13d69a168fa8268

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1023,7 +1023,7 @@ static __always_inline bool static_branch_##name(struct static_key *key) \
 extern struct static_key sched_feat_keys[__SCHED_FEAT_NR];
 #define sched_feat(x) (static_branch_##x(&sched_feat_keys[__SCHED_FEAT_##x]))
 #else /* !(SCHED_DEBUG && HAVE_JUMP_LABEL) */
-#define sched_feat(x) (sysctl_sched_features & (1UL << __SCHED_FEAT_##x))
+#define sched_feat(x) !!(sysctl_sched_features & (1UL << __SCHED_FEAT_##x))
 
 /*
  * Each translation unit has its own copy of sysctl_sched_features to allow
