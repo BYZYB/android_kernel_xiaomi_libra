7c82cd0623b6547fa8a4bafce10440f0490a420f
GuaiYiHu <guaiyihu@foxmail.com>
2019-01-06 22:56:04 +0800

    Revert "input: touchscreen: atmel_mxt: Allow disabling virtual keys"
    This reverts commit 4fbe21e9256057460196f5cbeb944216cc4c3008.
    
    Change-Id: Idf58ed45207264fcd7bc517cdf71e707e1953c4b

--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -699,7 +699,6 @@ struct mxt_data {
 	u8 lockdown_info[MXT_LOCKDOWN_SIZE];
 	u8 config_info[MXT_CONFIG_INFO_SIZE];
 	char *raw_ref_buf;
-	bool keys_off;
 
 	/* Slowscan parameters	*/
 	int slowscan_enabled;
@@ -1580,10 +1579,6 @@ static void mxt_proc_t15_messages(struct mxt_data *data, u8 *msg)
 
 	if (!input_dev)
 		return;
-
-        if(data->keys_off) {
-                return;
-        }
 
 	for (key = 0; key < pdata->config_array[index].key_num; key++) {
 		curr_state = test_bit(key, &data->keystatus);
@@ -3774,36 +3769,6 @@ static ssize_t mxt_pause_store(struct device *dev,
 	}
 }
 
-static ssize_t mxt_keys_off_show(struct device *dev,
-	struct device_attribute *attr, char *buf)
-{
-	struct mxt_data *data = dev_get_drvdata(dev);
-	int count;
-	char c;
-
-	c = data->keys_off ? '1' : '0';
-	count = sprintf(buf, "%c\n", c);
-
-	return count;
-}
-
-static ssize_t mxt_keys_off_store(struct device *dev,
-	struct device_attribute *attr, const char *buf, size_t count)
-{
-	struct mxt_data *data = dev_get_drvdata(dev);
-	int i;
-
-	if (sscanf(buf, "%u", &i) == 1 && i < 2) {
-		data->keys_off = (i == 1);
-
-		dev_dbg(dev, "%s\n", i ? "hw keys off" : "hw keys on");
-		return count;
-	} else {
-		dev_dbg(dev, "keys_off write error\n");
-		return -EINVAL;
-	}
-}
-
 static ssize_t mxt_debug_enable_show(struct device *dev,
 	struct device_attribute *attr, char *buf)
 {
@@ -5193,8 +5158,6 @@ static DEVICE_ATTR(debug_enable, S_IWUSR | S_IRUSR, mxt_debug_enable_show,
 			mxt_debug_enable_store);
 static DEVICE_ATTR(pause_driver, S_IWUSR | S_IRUSR, mxt_pause_show,
 			mxt_pause_store);
-static DEVICE_ATTR(keys_off, S_IWUSR | S_IRUSR, mxt_keys_off_show,
-			mxt_keys_off_store);
 static DEVICE_ATTR(version, S_IRUGO, mxt_version_show, NULL);
 static DEVICE_ATTR(build, S_IRUGO, mxt_build_show, NULL);
 static DEVICE_ATTR(slowscan_enable, S_IWUSR | S_IRUSR,
@@ -5217,7 +5180,6 @@ static struct attribute *mxt_attrs[] = {
 	&dev_attr_debug_enable.attr,
 	&dev_attr_pause_driver.attr,
 	&dev_attr_version.attr,
-	&dev_attr_keys_off.attr,
 	&dev_attr_build.attr,
 	&dev_attr_slowscan_enable.attr,
 	&dev_attr_self_tune.attr,
