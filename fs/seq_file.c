2ba9d3e2f03b136882666e70c99e5eb20ca392e0
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-09 22:28:05 +0900

    seq_file: use kmem_cache pool for single_open() and single_close()
    These get allocated and freed millions of times on Android.
    
    Use a dedicated kmem_cache pool and avoid costly dynamic memory allocations.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- fs/seq_file.c
+++ fs/seq_file.c
@@ -17,6 +17,7 @@
 #include <asm/page.h>
 
 static struct kmem_cache *seq_file_cache;
+static struct kmem_cache *seq_ops_cache;
 
 /*
  * seq_files have a buffer which can may overflow. When this happens a larger
@@ -602,7 +603,7 @@ static void single_stop(struct seq_file *p, void *v)
 int single_open(struct file *file, int (*show)(struct seq_file *, void *),
 		void *data)
 {
-	struct seq_operations *op = kmalloc(sizeof(*op), GFP_KERNEL);
+	struct seq_operations *op = kmem_cache_alloc(seq_ops_cache, GFP_KERNEL);
 	int res = -ENOMEM;
 
 	if (op) {
@@ -614,7 +615,7 @@ int single_open(struct file *file, int (*show)(struct seq_file *, void *),
 		if (!res)
 			((struct seq_file *)file->private_data)->private = data;
 		else
-			kfree(op);
+			kmem_cache_free(seq_ops_cache, op);
 	}
 	return res;
 }
@@ -642,7 +643,7 @@ int single_release(struct inode *inode, struct file *file)
 {
 	const struct seq_operations *op = ((struct seq_file *)file->private_data)->op;
 	int res = seq_release(inode, file);
-	kfree(op);
+	kmem_cache_free(seq_ops_cache, (void*)op);
 	return res;
 }
 EXPORT_SYMBOL(single_release);
@@ -961,4 +962,5 @@ EXPORT_SYMBOL(seq_hlist_next_rcu);
 void __init seq_file_init(void)
 {
 	seq_file_cache = KMEM_CACHE(seq_file, SLAB_PANIC);
+	seq_ops_cache = KMEM_CACHE(seq_operations, SLAB_HWCACHE_ALIGN | SLAB_PANIC);
 }
