2a38f6931de158ece2a2cc20226e340b8baa957a
Sultan Alsawaf <sultan@kerneltoast.com>
2019-07-30 14:19:59 -0700

    msm: kgsl: Avoid dynamically allocating small command buffers
    Most command buffers here are rather small (fewer than 256 words); it's
    a waste of time to dynamically allocate memory for such a small buffer
    when it could easily fit on the stack.
    
    Conditionally using an on-stack command buffer when the size is small
    enough eliminates the need for using a dynamically-allocated buffer most
    of the time, reducing GPU command submission latency.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- drivers/gpu/msm/adreno_ringbuffer.c
+++ drivers/gpu/msm/adreno_ringbuffer.c
@@ -1352,6 +1352,7 @@ int adreno_ringbuffer_submitcmd(struct adreno_device *adreno_dev,
 	struct kgsl_memobj_node *ib;
 	unsigned int numibs = 0;
 	unsigned int *link;
+	unsigned int link_onstack[SZ_256] __aligned(sizeof(long));
 	unsigned int *cmds;
 	struct kgsl_context *context;
 	struct adreno_context *drawctxt;
@@ -1452,10 +1453,14 @@ int adreno_ringbuffer_submitcmd(struct adreno_device *adreno_dev,
 		dwords += 6;
 	}
 
-	link = kzalloc(sizeof(unsigned int) *  dwords, GFP_KERNEL);
-	if (!link) {
-		ret = -ENOMEM;
-		goto done;
+	if (dwords <= ARRAY_SIZE(link_onstack)) {
+		link = link_onstack;
+	} else {
+		link = kmalloc(sizeof(unsigned int) * dwords, GFP_KERNEL);
+		if (!link) {
+			ret = -ENOMEM;
+			goto done;
+		}
 	}
 
 	cmds = link;
@@ -1578,7 +1583,8 @@ done:
 			numibs, cmdbatch->timestamp,
 			cmdbatch->flags, ret, drawctxt->type);
 
-	kfree(link);
+	if (link != link_onstack)
+		kfree(link);
 	return ret;
 }
 
