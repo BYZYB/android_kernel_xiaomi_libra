a80bb73e608f8d0996d61ee70122e553e7d2eeec
WJXXBSH <zyb_1998@outlook.com>
2019-11-07 13:09:19 +0800

    sched: inline with upstream eas [1/2]
    The source toddkjos has backported was in the early_stage of eas; update to the latest 3.18 backport.
    
    source: https://git.linaro.org/arm/eas/kernel.git/?h=linux-3.18-eas-5.2-plus
    
    Change-Id: Iccd5ce22e3afe4407fb8273ff3533493f15444b1
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>
    Signed-off-by: WJXXBSH < >

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -290,6 +290,14 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
 	},
+#if 0
+	{
+		.procname	= "sched_wake_to_idle",
+		.data		= &sysctl_sched_wake_to_idle,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
+	},
 	{
 		.procname	= "sched_capacity_margin",
 		.data		= &sysctl_sched_capacity_margin,
@@ -297,6 +305,7 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
 	},
+#endif
 #ifdef CONFIG_SCHED_FREQ_INPUT
 	{
 		.procname	= "sched_freq_inc_notify",
