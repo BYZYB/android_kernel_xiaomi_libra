b0b84a59f1875f312f818dbdf25deae9dd141e2e
WJXXBSH <zyb_1998@outlook.com>
2019-12-13 13:01:41 +0800

    BACKPORT: crypto: Add zstd support
    Change-Id: Id537a7ba88604e8ef152ad5bb8a2c27934d7bab5

--- crypto/crypto_user.c
+++ crypto/crypto_user.c
@@ -92,6 +92,21 @@ nla_put_failure:
 	return -EMSGSIZE;
 }
 
+static int crypto_report_acomp(struct sk_buff *skb, struct crypto_alg *alg)
+{
+	struct crypto_report_acomp racomp;
+
+	strncpy(racomp.type, "acomp", sizeof(racomp.type));
+
+	if (nla_put(skb, CRYPTOCFGA_REPORT_ACOMP,
+		    sizeof(struct crypto_report_acomp), &racomp))
+		goto nla_put_failure;
+	return 0;
+
+nla_put_failure:
+	return -EMSGSIZE;
+}
+
 static int crypto_report_comp(struct sk_buff *skb, struct crypto_alg *alg)
 {
 	struct crypto_report_comp rcomp;
