a1b35773770f3bad38e46ec858dd16d0f18249b0
mortezadadgar <mortezadadgar97@gmail.com>
2020-02-03 22:02:21 +0330

    msm: camera: Kill logging spam
    Change-Id: I9cd3e1173d18c81b0fe204a1b171a32844aacea4
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -2656,7 +2656,7 @@ void msm_isp_process_axi_irq(struct vfe_device *vfe_dev,
 			stream_info = &axi_data->stream_info[stream_idx];
 
 			if (stream_info->state == INACTIVE) {
-				pr_warn("%s: Warning! Stream already inactive. Drop irq handling\n",
+				pr_debug("%s: Warning! Stream already inactive. Drop irq handling\n",
 					__func__);
 				continue;
 			}
@@ -2705,7 +2705,7 @@ void msm_isp_process_axi_irq(struct vfe_device *vfe_dev,
 			stream_info = &axi_data->stream_info[stream_idx];
 
 			if (stream_info->state == INACTIVE) {
-				pr_warn("%s: Warning! Stream already inactive. Drop irq handling\n",
+				pr_debug("%s: Warning! Stream already inactive. Drop irq handling\n",
 					__func__);
 				continue;
 			}
