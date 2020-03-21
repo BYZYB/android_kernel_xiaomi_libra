c81f7f442d7825104d15ed82a131d31f507f7068
mortezadadgar <mortezadadgar97@gmail.com>
2020-03-21 13:49:25 +0430

    cpu-boost: Make remove wq bound
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -373,8 +373,8 @@ static void do_input_boost(struct work_struct *work)
 			sched_boost_active = true;
 	}
 
-	queue_delayed_work(cpu_boost_wq, &input_boost_rem,
-					msecs_to_jiffies(input_boost_ms));
+	schedule_delayed_work(&input_boost_rem,
+				    msecs_to_jiffies(input_boost_ms));
 }
 
 static void cpuboost_input_event(struct input_handle *handle,
