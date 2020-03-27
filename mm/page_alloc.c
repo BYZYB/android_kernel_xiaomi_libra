2751db442afbe3c78448cd4ea359f2691064ebc0
Sultan Alsawaf <sultan@kerneltoast.com>
2020-03-26 20:50:11 -0700

    mm: Don't warn on page allocation failures for OOM-killed processes
    It can be normal for a dying process to have its page allocation request
    fail when it has an OOM or LMK kill pending. In this case, it's actually
    detrimental to print out a massive allocation failure message because
    this means the running process needs to die quickly and release its
    memory, which is slowed down slightly by the massive kmsg splat. The
    allocation failure message is also a false positive in this case, since
    the failure is intentional rather than being the result of an inability
    to allocate memory.
    
    Suppress the allocation failure warning for processes that are killed to
    release memory in order to expedite their death and remedy the kmsg
    confusion from seeing spurious allocation failure messages.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2695,8 +2695,10 @@ rebalance:
 		goto nopage;
 
 	/* Avoid allocations with no watermarks from looping endlessly */
-	if (test_thread_flag_relaxed(TIF_MEMDIE) && !(gfp_mask & __GFP_NOFAIL))
+	if (test_thread_flag_relaxed(TIF_MEMDIE) && !(gfp_mask & __GFP_NOFAIL)) {
+		gfp_mask |= __GFP_NOWARN;
 		goto nopage;
+	}
 
 	/*
 	 * Try direct compaction. The first pass is asynchronous. Subsequent
