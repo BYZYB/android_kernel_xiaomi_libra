d9ad32b7e8cdfca97d10798fcdb22c96cb062823
BYZYB <zyb_1998@outlook.com>
2021-04-22 13:38:17 +0800

    libra_defconfig: Enable schedtune support
    Android could use cpuctl/cpuset/schedtune to adjust the priority of different processes.
    The first two ones are enabled by default, but schedtune was disabled in commit 11fcc5ca84455f2bc2b23a425395a994e493782b.
    Enable this feature again to improve the efficiency of energy-aware CPU schedulers (on supported Android versions).
    
    Change-Id: I517278bc54ef59973442a7127cc4f2a96fd322a0

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -98,11 +98,9 @@ static int try_to_freeze_tasks(bool user_only)
 	elapsed_msecs = elapsed_msecs64;
 
 	if (wakeup) {
-		printk("\n");
 		printk(KERN_ERR "Freezing of tasks aborted after %d.%03d seconds",
 		       elapsed_msecs / 1000, elapsed_msecs % 1000);
 	} else if (todo) {
-		printk("\n");
 		printk(KERN_ERR "Freezing of tasks failed after %d.%03d seconds"
 		       " (%d tasks refusing to freeze, wq_busy=%d):\n",
 		       elapsed_msecs / 1000, elapsed_msecs % 1000,
